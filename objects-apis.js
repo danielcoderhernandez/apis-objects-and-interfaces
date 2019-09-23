1209867721d3a45117f88f25afa7180362c0dd8a
Daniel Hernandez <dhernandez43@cnm.edu>
2019-09-23 17:51:06 -0600

    test

---
+++ objects-apis.js
@@ -0,0 +1,15 @@
+/*
+ * Objects, Interfaces, and API's
+ */
+
+let dog = {
+	name: "Steve",
+	color: "brown",
+	breed: "Pug",
+	size: "Small",
+	bark: function() {
+		console.log("Bark!");
+	},
+};
+
+dog.bark();
